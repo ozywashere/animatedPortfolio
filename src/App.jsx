6889bb5ecd8ed16547702d598cff98cba1f108b8
ozywashere <durduarslantas@ogr.iu.edu.tr>
2023-10-12 23:27:48 +0300

    animated portfolio project created

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./app.scss"
+import { Navbar } from "./navbar/Navbar.jsx";
+
+
+
+function App () {
+  
+  return ( <div>
+      <section>
+        <Navbar />
+
+      </section>
+      <section>Home</section>
+      <section>Projects</section>
+      <section>Skills</section>
+      <section>Experience</section>
+      <section>Education</section>
+      <section>Contact</section>
+    </div> )
+}
+
+export default App
---
+++ src/sidebar/toggleButton/ToggleButton.jsx
@@ -0,0 +1,37 @@
+import { motion } from "framer-motion";
+
+export const ToggleButton = ( { setOpen } ) => {
+  return ( <button onClick={ () => setOpen ( ( prev ) => ! prev ) }>
+    <svg width="24" height="24" viewBox="0 0 24 24">
+      <motion.path
+        d="M 3 2.5 L 20 2.5"
+        stroke="black"
+        strokeWidth="3"
+        strokeLinecap="round"
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" }
+        }}
+      />
+      <motion.path
+         d="M 2 9.423 L 20 9.423"
+        stroke="black"
+        strokeWidth="3"
+        strokeLinecap="round"
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" }
+        }}
+      />
+      <motion.path
+        stroke="black"
+        strokeWidth="3"
+        strokeLinecap="round"
+        variants={{
+          closed: { d: "M 2 16.346 L 20 16.346" },
+          open: { d: "M 3 2.5 L 17 16.346" }
+        }}
+      />
+    </svg>
+  </button> )
+}
